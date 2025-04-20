d82f2071af27ddd600355ee7634fb503c1139ce9
Yash Baberwal <yash147301@gmail.com>
2025-04-20 15:59:53 +0530

    user entity

---
+++ auth-service/src/main/java/Auth/Service/entity/User.java
@@ -0,0 +1,25 @@
+package Auth.Service.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String username;
+	private String password;
+	private String role;
+	
+}
